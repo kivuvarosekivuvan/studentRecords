84757d67ed54b2d397d45775baa28dba6842dbce
Rose <rosekivuvan@gmail.com>
2023-02-03 08:46:20 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var fullName: String = "Rose Joan"
+    println(fullName)
+    var age: Byte = 22
+    println(age)
+    var phoneNumber: String = "0797239759"
+    println(phoneNumber)
+    var weightInKg: Byte = 60
+    println(weightInKg)
+    var citizenshipKenyan: Boolean = true
+    var citizenshipNotKenyan: Boolean = false
+    println(citizenshipKenyan)
+    println(citizenshipNotKenyan)
+}
+
